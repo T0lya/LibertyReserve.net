2ea038ace22e3db119900afe83ca79c0694a704b
Anatoliy Dudarchuk <dudarchuk@gmail.com>
2011-07-10 12:22:32 +0300

    Added LRDemo project.

--- LibertyReserve/Requests.cs
+++ LibertyReserve/Requests.cs
@@ -1,11 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Xml.Linq;
 using System.Net;
 using System.Text;
 using System.Web;
-using System.IO;
+using System.Xml.Linq;
 
 namespace Magnis.Web.Services.LibertyReserve
 {
